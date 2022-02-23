ea683fc84e492db7085cd48787263fa4d8e67656
WY K <ablack1029@gmail.com>
2022-02-23 16:34:01 +0900

    Add : navbar구현

---
+++ src/components/Nav.tsx
@@ -0,0 +1,17 @@
+import "./Nav.scss";
+
+const Nav = () => {
+  return (
+    <div className='wrap'>
+      <img className='logo' src='../../img/logo.png' alt='logo' />
+      <div className='sideWrap'>
+        <img className='icon' src='../../img/Vector.png' alt='icon' />
+        <div className='menu'>A 가공 업체</div>
+        <img className='divider' src='../../img/divider.png' alt='divider' />
+        <div className='menu'>로그아웃</div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
